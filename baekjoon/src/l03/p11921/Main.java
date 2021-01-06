e63298e17df20d23e4ae7848fc912a93badfe0b7
yehwankim23 <yehwankim23@gmail.com>
2021-01-06 23:29:09 +0900

    Edit problem 11921

--- baekjoon/src/l03/p11921/Main.java
+++ baekjoon/src/l03/p11921/Main.java
@@ -2,9 +2,9 @@ package l03.p11921;
 
 // 주소: https://www.acmicpc.net/problem/11921
 // 제목: 0.1
-// 결과: 0.2점
-// 메모리: 11692 KB
-// 시간: 84 ms
+// 결과: 2점
+// 메모리: 13628 KB
+// 시간: 92 ms
 
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
@@ -13,12 +13,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedWriter bw
                 = new BufferedWriter(new OutputStreamWriter(System.out));
-        byte[] buffer = new byte[100000];
+        byte[] buffer = new byte[1000000];
         System.in.read(buffer, 0, buffer.length);
         int index = 8;
         long sum = 0;
         
-        for (int i = 0, n = 0, b; i < 10000; i++, n = 0) {
+        for (int i = 0, n = 0, b; i < 100000; i++, n = 0) {
             while ((b = buffer[index++]) != '\n') {
                 n *= 10;
                 n += b - '0';
@@ -27,7 +27,7 @@ public class Main {
             sum += n;
         }
         
-        bw.write("10000\n".concat(Long.toString(sum)));
+        bw.write("100000\n".concat(Long.toString(sum)));
         bw.flush();
     }
 }
