ffc870f673085d4fa701cccc623de8c86a6c3d1b
yehwankim23 <yehwankim23@gmail.com>
2021-01-06 23:36:43 +0900

    Edit problem 11921

--- baekjoon/src/l03/p11921/Main.java
+++ baekjoon/src/l03/p11921/Main.java
@@ -2,24 +2,23 @@ package l03.p11921;
 
 // 주소: https://www.acmicpc.net/problem/11921
 // 제목: 0.1
-// 결과: 시간 초과
-// 메모리: - KB
-// 시간: - ms
+// 결과: 5점
+// 메모리: 16708 KB
+// 시간: 100 ms
 
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 
-// FIXME - 0.1
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedWriter bw
                 = new BufferedWriter(new OutputStreamWriter(System.out));
-        byte[] buffer = new byte[5000000];
+        byte[] buffer = new byte[2500000];
         System.in.read(buffer, 0, buffer.length);
         int index = 8;
         long sum = 0;
         
-        for (int i = 0, n = 0, b; i < 500000; i++, n = 0) {
+        for (int i = 0, n = 0, b; i < 250000; i++, n = 0) {
             while ((b = buffer[index++]) != '\n') {
                 n *= 10;
                 n += b - '0';
@@ -28,7 +27,7 @@ public class Main {
             sum += n;
         }
         
-        bw.write("500000\n".concat(Long.toString(sum)));
+        bw.write("250000\n".concat(Long.toString(sum)));
         bw.flush();
     }
 }
