5e8b10dc4cb7fd777fe82cfdbf33e4b3f15fd7ed
yehwankim23 <yehwankim23@gmail.com>
2021-01-07 18:31:51 +0900

    Edit problem 11921

--- baekjoon/src/l03/p11921/Main.java
+++ baekjoon/src/l03/p11921/Main.java
@@ -2,24 +2,28 @@ package l03.p11921;
 
 // 주소: https://www.acmicpc.net/problem/11921
 // 제목: 0.1
-// 결과: 5점
-// 메모리: 15500 KB
-// 시간: 100 ms
+// 결과: 틀렸습니다
+// 메모리: - KB
+// 시간: - ms
 
+// FIXME - 0.1
 public class Main {
     public static void main(String[] args) throws Exception {
         byte[] buffer = new byte[2000000];
         System.in.read(buffer, 0, 2000000);
-        int index = 8;
+        int i = 0, temp = 0, index = 8;
+        byte b;
         long sum = 0;
         
-        for (int i = 0, n = 0, b; i < 250000; i++, n = 0) {
-            while ((b = buffer[index++]) != 10) {
-                n *= 10;
-                n += b - 48;
+        while (i++ < 250000) {
+            temp = buffer[index++] - 48;
+            
+            while ((b = buffer[index++]) != '\n') {
+                temp *= 10;
+                temp += b - 48;
             }
             
-            sum += n;
+            sum += temp;
         }
         
         buffer[0] = '2';
@@ -29,17 +33,19 @@ public class Main {
         buffer[4] = '0';
         buffer[5] = '0';
         buffer[6] = '\n';
-        index = 0;
+        temp = 16;
         
         while (sum > 0) {
-            buffer[20 + index++] = (byte) (sum % 10 + 48);
+            buffer[temp++] = (byte) (sum % 10 + 48);
             sum /= 10;
         }
         
-        for (int i = 0; i < index; i++) {
-            buffer[7 + i] = buffer[19 + index - i];
+        index = 7;
+        
+        while (temp-- > 16) {
+            buffer[index++] = buffer[temp];
         }
         
-        System.out.write(buffer, 0, 7 + index);
+        System.out.write(buffer, 0, index);
     }
 }
