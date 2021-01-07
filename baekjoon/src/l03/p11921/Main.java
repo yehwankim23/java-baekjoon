387c9c5caa01ffbf9981869cb7ff2767b9401e7e
yehwankim23 <yehwankim23@gmail.com>
2021-01-07 11:39:05 +0900

    Edit problem 11921

--- baekjoon/src/l03/p11921/Main.java
+++ baekjoon/src/l03/p11921/Main.java
@@ -2,32 +2,41 @@ package l03.p11921;
 
 // 주소: https://www.acmicpc.net/problem/11921
 // 제목: 0.1
-// 결과: 5점
-// 메모리: 16708 KB
-// 시간: 100 ms
+// 결과: 시간 초과
+// 메모리: - KB
+// 시간: - ms
 
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-
+// FIXME - 0.1
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedWriter bw
-                = new BufferedWriter(new OutputStreamWriter(System.out));
-        byte[] buffer = new byte[2500000];
-        System.in.read(buffer, 0, buffer.length);
+        byte[] buffer = new byte[5000000];
+        System.in.read(buffer, 0, 5000000);
         int index = 8;
         long sum = 0;
         
-        for (int i = 0, n = 0, b; i < 250000; i++, n = 0) {
-            while ((b = buffer[index++]) != '\n') {
+        for (int i = 0, n = 0, b; i < 500000; i++, n = 0) {
+            while ((b = buffer[index++]) != 10) {
                 n *= 10;
-                n += b - '0';
+                n += b - 48;
             }
             
             sum += n;
         }
         
-        bw.write("250000\n".concat(Long.toString(sum)));
-        bw.flush();
+        buffer[0] = '5';
+        buffer[1] = '0';
+        buffer[2] = '0';
+        buffer[3] = '0';
+        buffer[4] = '0';
+        buffer[5] = '0';
+        buffer[6] = '\n';
+        index = (int) (Math.log10(sum) + 1);
+        
+        for (int i = index; i > 0; i--) {
+            buffer[6 + i] = (byte) (sum % 10 + 48);
+            sum /= 10;
+        }
+        
+        System.out.write(buffer, 0, 7 + index);
     }
 }
