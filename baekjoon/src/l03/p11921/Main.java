d59818dd6e90b5a80e7ec17fee8060216441d28e
yehwankim23 <yehwankim23@gmail.com>
2021-01-07 11:41:51 +0900

    Edit problem 11921

--- baekjoon/src/l03/p11921/Main.java
+++ baekjoon/src/l03/p11921/Main.java
@@ -2,19 +2,18 @@ package l03.p11921;
 
 // 주소: https://www.acmicpc.net/problem/11921
 // 제목: 0.1
-// 결과: 시간 초과
-// 메모리: - KB
-// 시간: - ms
+// 결과: 5점
+// 메모리: 16468 KB
+// 시간: 100 ms
 
-// FIXME - 0.1
 public class Main {
     public static void main(String[] args) throws Exception {
-        byte[] buffer = new byte[5000000];
-        System.in.read(buffer, 0, 5000000);
+        byte[] buffer = new byte[2500000];
+        System.in.read(buffer, 0, 2500000);
         int index = 8;
         long sum = 0;
         
-        for (int i = 0, n = 0, b; i < 500000; i++, n = 0) {
+        for (int i = 0, n = 0, b; i < 250000; i++, n = 0) {
             while ((b = buffer[index++]) != 10) {
                 n *= 10;
                 n += b - 48;
@@ -23,8 +22,8 @@ public class Main {
             sum += n;
         }
         
-        buffer[0] = '5';
-        buffer[1] = '0';
+        buffer[0] = '2';
+        buffer[1] = '5';
         buffer[2] = '0';
         buffer[3] = '0';
         buffer[4] = '0';
