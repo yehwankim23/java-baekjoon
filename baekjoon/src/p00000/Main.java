962ee6628a2235fc463d00dc1e5682f00a32e9ef
yehwankim23 <yehwankim23@gmail.com>
2021-03-13 16:53:58 +0900

    Edit template

--- baekjoon/src/p00000/Main.java
+++ baekjoon/src/p00000/Main.java
@@ -6,8 +6,14 @@ package p00000;
 // 메모리: - KB
 // 시간: - ms
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        //
+    public static void main(String[] args) throws IOException {
+        BufferedReader br
+                = new BufferedReader(new InputStreamReader(System.in));
+        br.close();
     }
 }
