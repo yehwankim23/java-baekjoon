4e1b793d5eccb6162bab7f851b9aba7cc0ccf33b
yehwankim23 <yehwankim23@gmail.com>
2021-03-13 17:25:21 +0900

    Edit problem 9633

--- baekjoon/src/p09663/Main.java
+++ baekjoon/src/p09663/Main.java
@@ -2,70 +2,52 @@ package p09663;
 
 // 주소: https://www.acmicpc.net/problem/9663
 // 제목: N-Queen
-// 결과: 메모리 초과
-// 메모리: - KB
-// 시간: - ms
+// 결과: 맞았습니다!!
+// 메모리: 12116 KB
+// 시간: 5916 ms
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     private static int n;
+    private static int[] board;
+    private static int count;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br
                 = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         br.close();
-        long start = System.nanoTime();
+        board = new int[n];
+        count = 0;
         
-        // FIXME - N-Queen
-        System.out.println(backtrack(new boolean[n][n], 0, 1));
+        backtrack(0);
         
-        long end = System.nanoTime();
-        System.out.println((end - start) / 1000000 + " ms");
+        System.out.println(count);
     }
     
-    private static int backtrack(boolean[][] board, int index, int queen) {
-        int count = 0;
-        
-        for (int i = index; i < n * n; i++) {
-            int row = i / n, column = i % n;
-            
-            if (!board[row][column]) {
-                if (queen == n) {
-                    count++;
-                } else {
-                    boolean[][] copy = new boolean[n][n];
-                    
-                    for (int j = 0; j < n; j++) {
-                        copy[j] = Arrays.copyOf(board[j], n);
+    private static void backtrack(int queen) {
+        if (queen == n) {
+            count++;
+        } else {
+            for (int column = 0; column < n; column++) {
+                boolean valid = true;
+                
+                for (int row = 0; row < queen; row++) {
+                    if (board[row] == column || Math.abs(queen - row)
+                            == Math.abs(column - board[row])) {
+                        valid = false;
+                        break;
                     }
-                    
-                    for (int j = 0; j < n; j++) {
-                        copy[row][j] = true;
-                        copy[j][column] = true;
-                        
-                        try {
-                            copy[row - column + j][j] = true;
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            //
-                        }
-                        
-                        try {
-                            copy[j][column + row - j] = true;
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            //
-                        }
-                    }
-                    
-                    count += backtrack(copy, i + 1, queen + 1);
+                }
+                
+                if (valid) {
+                    board[queen] = column;
+                    backtrack(queen + 1);
                 }
             }
         }
-        
-        return count;
     }
 }
