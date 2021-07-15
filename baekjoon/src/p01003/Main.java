6cee0239270ed98ed433c83f87ba57281ca695ab
yehwankim23 <yehwankim23@gmail.com>
2021-07-15 22:37:58 +0900

    Edit problem 1003

--- baekjoon/src/p01003/Main.java
+++ baekjoon/src/p01003/Main.java
@@ -2,7 +2,7 @@ package p01003;
 
 // 주소: https://www.acmicpc.net/problem/1003
 // 제목: 피보나치 함수
-// 결과: 시간 초과
+// 결과: 틀렸습니다
 // 메모리: - KB
 // 시간: - ms
 
@@ -11,39 +11,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    private static int zero;
-    private static int one;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br
                 = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
+        int[][] cache = new int[41][2];
+
+        for (int i = 0, prev = 0, curr = 1; i < 11; i++) {
+            cache[i][0] = curr - prev;
+            cache[i][1] = prev;
+
+            curr += prev;
+            prev = curr - prev;
+        }
+
+        cache[0][0] = 1;
         StringBuilder sb = new StringBuilder();
 
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            zero = 0;
-            one = 0;
 
-            fibonacci(n);
-
-            sb.append(zero).append(" ").append(one).append("\n");
+            sb.append(cache[n][0]).append(" ").append(cache[n][1]).append("\n");
         }
 
         br.close();
         System.out.print(sb);
-    }
-
-    private static void fibonacci(int n) {
-        if (n == 0) {
-            zero++;
-            return;
-        } else if (n == 1) {
-            one++;
-            return;
-        } else {
-            fibonacci(n - 1);
-            fibonacci(n - 2);
-        }
     }
 }
